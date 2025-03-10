de3ce709e54b4a70446dac8132036fb702e6eefb
hamim <hamim2114@gmail.com>
2025-03-10 17:38:02 +0600

    updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,3 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from 'react-responsive-carousel';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
